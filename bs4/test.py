593a5a6b6169acde293d7606e3471e55a9ec5d26
SevenXue <825056849@qq.com>
2017-04-10 23:24:57 +0800

    beautiful soup

---
+++ bs4/test.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get("http://python123.io/ws/demo.html")
+
+demo = r.text
+
+soup = BeautifulSoup(demo, 'html.parser')
+print(soup.prettify())
+
