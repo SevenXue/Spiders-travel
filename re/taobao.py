d8945094c66f06388788293a6754d9c42f6b924f
SevenXue <825056849@qq.com>
2017-04-14 22:05:45 +0800

    re
    正则表达式

---
+++ re/taobao.py
@@ -0,0 +1,19 @@
+import re
+import requests
+
+def getHtmlText(url):
+    print()
+
+def parsePage(ilt, html):
+    print()
+
+def printGoodsList(ilt):
+    print()
+
+def main():
+    goods = '书包'
+    depth = 2
+    start_url = 'https://s.taobao.com/search?q=' + goods
+    infoList = []
+    for i in range(depth):
+        infoList[i] = i*44
