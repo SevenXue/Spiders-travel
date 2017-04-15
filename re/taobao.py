eb12da7fe1054cfd5bd76715a77146ef64f2115c
SevenXue <825056849@qq.com>
2017-04-15 09:45:43 +0800

    re+taobao
    对淘宝商品价格进行爬虫，使用两种不同的正则表达式和信息提取方法

--- re/taobao.py
+++ re/taobao.py
@@ -1,14 +1,50 @@
 import re
 import requests
 
+#~ 关注parsePage()，使用两种不同的正则表达式
+
 def getHtmlText(url):
-    print()
+    try:
+        r = requests.get(url, timeout = 30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
 
 def parsePage(ilt, html):
-    print()
+    try:
+        plt = re.findall(r'"view_price":"(\d+\.\d+)"', html)
+        tlt = re.findall(r'"raw_title":"(.*?)"', html)
+        for i in range(len(plt)):
+            price = plt[i]
+            title = tlt[i]
+            ilt.append([price,title])
+    except:
+        print("error")
+
+    '''
+    #不同的正则表达式，一样的效果
+    try:
+        plt = re.findall(r'\"view_price\"\:\"[\d\.]*\"', html)
+        tlt = re.findall(r'\"raw_title\"\:\".*?\"', html)
+        for i in range(len(plt)):
+            #eval去掉双引号
+            price = eval(plt[i].split(':')[1])
+            title = eval(tlt[i].split(':')[1])
+            ilt.append([price,title])
+    except:
+        print("error")
+    '''
+
 
 def printGoodsList(ilt):
-    print()
+    tplt = "{:4}\t{:8}\t{:16}"
+    print(tplt.format("序号", "价格", "商品名称"))
+    count = 0
+    for g in ilt:
+        count += 1
+        print(tplt.format(count, g[0], g[1]))
 
 def main():
     goods = '书包'
@@ -22,4 +58,8 @@ def main():
             parsePage(infoList, html)
         except:
             continue
-            
+    printGoodsList(infoList)
+
+if __name__ == '__main__':
+    main()
+
